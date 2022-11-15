d1dc77017c8cd42772a829e47eaedfd5451ec036
Dušan Stokić <69325657+StokicDusan@users.noreply.github.com>
2022-11-15 20:46:34 +0100

    minor formating

--- arithmeticSequence.py
+++ arithmeticSequence.py
@@ -1,6 +1,6 @@
 def arithmeticSequence(A,n):
-    SubSequence=[]
-    ArithmeticSequences=[]
+    SubSequence = []
+    ArithmeticSequences = []
     #Create array of pairs from array A
     for index,item in enumerate(A[:-1]):
         for index2,item2 in enumerate(A[index+1:]):
@@ -10,7 +10,7 @@ def arithmeticSequence(A,n):
         if (pair[1] - pair[0] == n):
             found = [pair[0],pair[1]]
             for index2,pair2 in enumerate(SubSequence[index+1:]):
-                if (pair2[0]==found[-1] and pair2[1]-pair2[0]==n):
+                if (pair2[0] == found[-1] and pair2[1]-pair2[0] == n):
                     found.append(pair2[1])
             if (len(found)>2): ArithmeticSequences.append(found)
     return ArithmeticSequences
@@ -25,12 +25,12 @@ def distinctArithmeticSequences(A):
 
 def main():
     df = [1,10,11,20,21,30,40,41,50,60,61,70]
-    common_differene=10
+    common_differene = 10
 
-    arseq=arithmeticSequence(df,common_differene)
+    arseq = arithmeticSequence(df,common_differene)
     print(arseq)
 
-    darseq=distinctArithmeticSequences(arseq)
+    darseq = distinctArithmeticSequences(arseq)
     print(darseq)
 
 if __name__ == "__main__":
