8f037bcf50a3134a1db798b4c81c7ab4a362639d
StokicDusan <stokicdusan@gmail.com>
2022-11-19 15:56:37 +0100

    fix distinctArithmetic functions

--- arithmeticSequence.py
+++ arithmeticSequence.py
@@ -36,32 +36,44 @@ def arithmeticSequences(A: list) -> list:
     return ArithmeticSequences
 
 def distArSeq(A: list) -> list:
-    DistinctArithmeticSequences = arithmeticSequences(A)
-    for index,item in enumerate(A):
-        for index2,item2 in enumerate([x for x in A if x != item]):
-            if (set(item2) <= set(item)):
+    seq = arithmeticSequences(A)
+    DistinctArithmeticSequences = seq
+    for index,item in enumerate(seq):
+        for index2,item2 in enumerate([x for x in seq if x != item]):
+            if (set(item2) <= set(item)) and (item[1] - item[0] == item2[1] - item2[0]):
                 DistinctArithmeticSequences.remove(item2)
     return DistinctArithmeticSequences
 
 def distArSeqCommon(A: list,n:int) -> list:
-    DistinctArithmeticSequences = arithmeticSequencesCommon(A,n)
-    for index,item in enumerate(A):
-        for index2,item2 in enumerate([x for x in A if x != item]):
-            if (set(item2) <= set(item)):
+    seq = arithmeticSequencesCommon(A,n)
+    DistinctArithmeticSequences = seq
+    for index,item in enumerate(seq):
+        for index2,item2 in enumerate([x for x in seq if x != item]):
+            if (set(item2) <= set(item)) and (item[1] - item[0] == item2[1] - item2[0]):
                 DistinctArithmeticSequences.remove(item2)
     return DistinctArithmeticSequences
 
 
-def main():
-    df = [1,10,11,20,21,30,40,41,50,60,61,70]
-    common_difference = 10
-
-    arseq = arithmeticSequences(df,common_difference)
-    print(arseq)
-
-    darseq = distinctArithmeticSequences(arseq)
-    print(darseq)
+def test_arithmeticSequence() -> None:
+    """
+    >>> arSeq = [1,10,30,40,80,110]
+    >>> distArSeq(arSeq)
+    []
+    >>> arSeq = [1,10,11,20,21,30,40,41,50,60,61,70]
+    >>> distArSeq(arSeq)
+    [[1, 11, 21], [1, 21, 41, 61], [10, 20, 30, 40, 50, 60, 70], [10, 30, 50, 70], [10, 40, 70], [20, 40, 60]]
+    >>> arSeq = [1,10,11,20,21,30,40,41,50,60,61,70]
+    >>> distArSeqCommon(arSeq,20)
+    [[1, 21, 41, 61], [10, 30, 50, 70], [20, 40, 60]]
+    >>> arSeq = [2,10,17,24,31,38,45,52,60]
+    >>> distArSeq(arSeq)
+    [[2, 31, 60], [10, 17, 24, 31, 38, 45, 52], [10, 24, 38, 52], [10, 31, 52], [17, 31, 45]]
+    >>> arSeq = [2,10,17,24,31,38,45,52,60]
+    >>> distArSeqCommon(arSeq,7)
+    [[10, 17, 24, 31, 38, 45, 52]]
+    """
+    pass
 
 if __name__ == "__main__":
-    main()
-
+    from doctest import testmod
+    testmod()
