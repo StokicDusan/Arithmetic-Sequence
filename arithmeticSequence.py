4d0914879318d47f476986c1baa9257c7f9bd8a1
StokicDusan <stokicdusan@gmail.com>
2022-11-19 15:01:03 +0100

    modify distinctArithmetic functions

--- arithmeticSequence.py
+++ arithmeticSequence.py
@@ -1,6 +1,6 @@
-from math import cell
+from math import ceil
 
-def arithmeticSequenceCommon(A: list,n:int) -> list:
+def arithmeticSequencesCommon(A: list,n:int) -> list:
     SubSequence = []
     ArithmeticSequences = []
     #Create array of pairs from array A
@@ -17,7 +17,7 @@ def arithmeticSequenceCommon(A: list,n:int) -> list:
             if (len(found)>2): ArithmeticSequences.append(found)
     return ArithmeticSequences
 
-def arithmeticSequence(A: list) -> list:
+def arithmeticSequences(A: list) -> list:
     SubSequence = []
     ArithmeticSequences = []
     #Create array of pairs from array A
@@ -26,7 +26,7 @@ def arithmeticSequence(A: list) -> list:
             SubSequence.append([item,item2])
     #finding arithmetic sequences
     for index,pair in enumerate(SubSequence):
-        for n in range(1,cell((A[-1]-A[0])/2)+1):
+        for n in range(1,ceil((A[-1]-A[0])/2)+1):
             if (pair[1] - pair[0] == n):
                 found = [pair[0],pair[1]]
                 for index2,pair2 in enumerate(SubSequence[index+1:]):
@@ -35,19 +35,28 @@ def arithmeticSequence(A: list) -> list:
                 if (len(found)>2): ArithmeticSequences.append(found)
     return ArithmeticSequences
 
-def distinctArithmeticSequences(A: list) -> list:
-    DistinctArithmeticSequences = A
+def distArSeq(A: list) -> list:
+    DistinctArithmeticSequences = arithmeticSequences(A)
     for index,item in enumerate(A):
         for index2,item2 in enumerate([x for x in A if x != item]):
             if (set(item2) <= set(item)):
                 DistinctArithmeticSequences.remove(item2)
     return DistinctArithmeticSequences
 
+def distArSeqCommon(A: list,n:int) -> list:
+    DistinctArithmeticSequences = arithmeticSequencesCommon(A,n)
+    for index,item in enumerate(A):
+        for index2,item2 in enumerate([x for x in A if x != item]):
+            if (set(item2) <= set(item)):
+                DistinctArithmeticSequences.remove(item2)
+    return DistinctArithmeticSequences
+
+
 def main():
     df = [1,10,11,20,21,30,40,41,50,60,61,70]
     common_difference = 10
 
-    arseq = arithmeticSequence(df,common_difference)
+    arseq = arithmeticSequences(df,common_difference)
     print(arseq)
 
     darseq = distinctArithmeticSequences(arseq)
