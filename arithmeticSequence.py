d5179309946ba6c56f7eaebc882871ca277034e9
StokicDusan <stokicdusan@gmail.com>
2022-11-19 14:07:01 +0100

    add function annotation

--- arithmeticSequence.py
+++ arithmeticSequence.py
@@ -1,4 +1,4 @@
-def arithmeticSequence(A,n):
+def arithmeticSequence(A: list,n:int) -> list:
     SubSequence = []
     ArithmeticSequences = []
     #Create array of pairs from array A
@@ -15,8 +15,8 @@ def arithmeticSequence(A,n):
             if (len(found)>2): ArithmeticSequences.append(found)
     return ArithmeticSequences
 
-def distinctArithmeticSequences(A):
-    DistinctArithmeticSequences = A
+def distinctArithmeticSequences(A: list) -> list:
+    DistinctArithmeticSequences = arithmeticSequence(A)
     for index,item in enumerate(A):
         for index2,item2 in enumerate([x for x in A if x != item]):
             if (set(item2) <= set(item)):
@@ -24,10 +24,10 @@ def distinctArithmeticSequences(A):
     return DistinctArithmeticSequences
 
 def main():
-    df = [1,10,11,20,21,30,40,41,50,60,61,70]
-    common_differene = 10
+    df = [1,10,11,20,21,30,40,41,50]
+    common_difference = 10
 
-    arseq = arithmeticSequence(df,common_differene)
+    arseq = arithmeticSequence(df,common_difference)
     print(arseq)
 
     darseq = distinctArithmeticSequences(arseq)
@@ -35,3 +35,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
