784717a04685bd6e1c4dee81e30f02a4200f1c26
StokicDusan <stokicdusan@gmail.com>
2022-11-19 14:15:28 +0100

    separate functions for arithmetic sequences with known common difference and all common differences

--- arithmeticSequence.py
+++ arithmeticSequence.py
@@ -1,4 +1,6 @@
-def arithmeticSequence(A: list,n:int) -> list:
+from math import cell
+
+def arithmeticSequenceCommon(A: list,n:int) -> list:
     SubSequence = []
     ArithmeticSequences = []
     #Create array of pairs from array A
@@ -15,8 +17,26 @@ def arithmeticSequence(A: list,n:int) -> list:
             if (len(found)>2): ArithmeticSequences.append(found)
     return ArithmeticSequences
 
+def arithmeticSequence(A: list) -> list:
+    SubSequence = []
+    ArithmeticSequences = []
+    #Create array of pairs from array A
+    for index,item in enumerate(A[:-1]):
+        for index2,item2 in enumerate(A[index+1:]):
+            SubSequence.append([item,item2])
+    #finding arithmetic sequences
+    for index,pair in enumerate(SubSequence):
+        for n in range(1,cell((A[-1]-A[0])/2)+1):
+            if (pair[1] - pair[0] == n):
+                found = [pair[0],pair[1]]
+                for index2,pair2 in enumerate(SubSequence[index+1:]):
+                    if (pair2[0] == found[-1] and pair2[1]-pair2[0] == n):
+                        found.append(pair2[1])
+                if (len(found)>2): ArithmeticSequences.append(found)
+    return ArithmeticSequences
+
 def distinctArithmeticSequences(A: list) -> list:
-    DistinctArithmeticSequences = arithmeticSequence(A)
+    DistinctArithmeticSequences = A
     for index,item in enumerate(A):
         for index2,item2 in enumerate([x for x in A if x != item]):
             if (set(item2) <= set(item)):
@@ -24,7 +44,7 @@ def distinctArithmeticSequences(A: list) -> list:
     return DistinctArithmeticSequences
 
 def main():
-    df = [1,10,11,20,21,30,40,41,50]
+    df = [1,10,11,20,21,30,40,41,50,60,61,70]
     common_difference = 10
 
     arseq = arithmeticSequence(df,common_difference)
